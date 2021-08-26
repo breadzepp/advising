7328801d3255506d62b14b441273406b218cc60c
Mike Beta <mike@mikebeta.com>
2021-08-25 20:31:52 -0700

    Gets needed courses and appends them to the top of the page

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// let changeColor = document.getElementById("changeColor");
+//
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
@@ -11,19 +11,46 @@ changeColor.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: getNeededCourses,
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
+
+function getNeededCourses() {
   // chrome.storage.sync.get("color", ({ color }) => {
   //   document.body.style.backgroundColor = color;
   // });
+  var neededCoursesElementId = 'NeededCourses';
+
+  // Check if we've already created the table
+  var neededCoursesElement = document. getElementById(neededCoursesElementId);
+  if (typeof(neededCoursesElement) != 'undefined' &&
+      neededCoursesElement != null) {
+    return;
+  }
+
+  // Get all needed courses
   var elms = document.querySelectorAll("[id='CoursesTable']");
+  var courses = [];
+  for (var i = 0; i < elms.length; i++) {
+    var rows = elms[i].getElementsByTagName("tr");
+    for (var j = 0; j < rows.length; j++) {
+      var columns = rows[j].getElementsByTagName("td");
+      for (var k = 0; k < columns.length; k++) {
+        if (columns[k].textContent == "1 course needed") {
+          courses.push(rows[j]);
+          break;
+        }
+      }
+    }
+  }
 
-  for(var i = 0; i < elms.length; i++)
-    console.log(elms[i]); // <-- whatever you need to do he
-
+  // Create needed courses table and append to top of the page
+  var insertHTML = '<table id="' + neededCoursesElementId + '">';
+  for (var i = 0; i < courses.length; i++) {
+    insertHTML += courses[i].outerHTML;
+  }
+  insertHTML += '</table><br /><b>Total Needed: </b>' + courses.length
+  document.getElementById("reportText").insertAdjacentHTML('afterbegin',
+    insertHTML);
 }
